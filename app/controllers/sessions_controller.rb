a04b786fa5fb0bed2f9f0419fbaabad8adc52f26
Oscar <shoudto@gmail.com>
2019-10-12 17:12:36 -0400

    created a login in page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,10 +1,21 @@
 class SessionsController < ApplicationController 
 
     #login form
-
+    get '/login' do 
+        erb :"sessions/login"
+    end
 
     #put_in_session
+    post '/sessions' do 
+        user = User.find_by(email: params[:email])
 
+        if user && user.authenticate(params[:password])
+            session[:user_id] = user.id 
+            redirect "/blogs"
+        else 
+            redirect "/login"
+        end 
+    end 
 
     #logout
     get '/logout' do 
