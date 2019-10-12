3cb071ef62d65504e136e84cbc738754408c7989
Oscar <shoudto@gmail.com>
2019-10-12 17:06:17 -0400

    created a logout function

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,14 @@
 class SessionsController < ApplicationController 
 
+    #login form
+
+
+    #put_in_session
+
+
+    #logout
+    get '/logout' do 
+        session.destroy
+        redirect "/"
+    end 
 end 
